7c2d0ae4060f0889dd447a96ca6999af4acce8d6
Kirsty Chatterton <chattertonkirsty@gmail.com>
2020-09-20 21:02:36 +0100

    img: added additional images to quiz questions/answers

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -81,7 +81,7 @@ function nextQuestion() {
         wellDone.classList.add('hide-content');
         quizContainer.classList.remove('hide-content');
 
-        if (availableQuestions.length == 0) {
+        if (availableQuestions.length == 0 || questionCounter == maxQuestions) {
             finish.classList.remove('hide-content');
             wellDone.classList.add('hide-content');
             quizContainer.classList.add('hide-content');
@@ -110,11 +110,11 @@ const questions = [{
         answer3: 'Cello',
         answer4: 'Flute',
         correct: '2', //Violin
-        message: `<img class="quiz-img" src="assets/images/violinist.jpg" alt="Man Playing the Violin"><p>It's the Violin.</p><p>The Violin is a member of The String Family.</p>`
+        message: `<img class="quiz-img" src="assets/images/violin-player.jpg" alt="Someone Playing the Violin"><p>It's the Violin.</p><p>The Violin is a member of The String Family.</p>`
     },
     {
         //2
-        question: `<h2>Which of these instruments has more than four strings?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        question: `<h2>Which of these instruments has more than four strings?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Harp',
         answer2: 'Cello',
         answer3: 'Oboe',
@@ -124,13 +124,13 @@ const questions = [{
     },
     {
         //3
-        question: `<h2>Which of these instruments sounds the lowest?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        question: `<h2>Which of these instruments sounds the lowest?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Trombone',
         answer2: 'French Horn',
         answer3: 'Clarinet',
         answer4: 'Double Bass',
         correct: '4', //Double Bass
-        message: `<img class="quiz-img" src="assets/images/double-bass.jpg" alt="Man Playing Double Bass"><p>It's the Double Bass!</p>`
+        message: `<img class="quiz-img" src="assets/images/double-bass-player.jpg" alt="Someone Playing Double Bass"><p>It's the Double Bass!</p>`
     },
     {
         //4
@@ -140,12 +140,12 @@ const questions = [{
         answer3: 'The Brass Family',
         answer: 'The Woodwind Family',
         correct: '2', //The String Family - Cello
-        message: `<img class="quiz-img" src="assets/images/cello-player.jpg" alt="Man Playing the Cello"><p>The Cello is a member of The String Family!</p>`
+        message: `<img class="quiz-img" src="assets/images/cello-player.jpg" alt="Someone Playing the Cello"><p>The Cello is a member of The String Family!</p>`
     },
     {
         //Woodwind Instruments
         //5
-        question: `<h2>Which of these instruments sounds the highest?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        question: `<h2>Which of these instruments sounds the highest?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Bassoon',
         answer2: 'French Horn',
         answer3: 'Flute',
@@ -155,155 +155,145 @@ const questions = [{
     },
     {
         //6
-        question: `<h2>Can you guess the name of this instrument?</h2>`,
+        question: `<h2>Can you guess the name of this instrument?</h2><img class="quiz-img" src="assets/images/oboe.jpg" alt="Oboe">`,
         answer1: 'Oboe',
         answer2: 'Flute',
         answer3: 'French Horn',
         answer4: 'Clarinet',
         correct: '1', //Oboe
-        message: `<p>It's the Oboe!</p>`
+        message: `<img class="quiz-img" src="assets/images/oboe-player.jpg" alt="Someone Playing the Oboe"><p>It's the Oboe!</p>`
     },
     {
         //7
-        question: `<h2>Which of these instruments is played with a double reed?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        question: `<h2>Which of these instruments is played with a double reed?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Clarinet',
         answer2: 'Flute',
         answer3: 'Bassoon',
         answer4: 'Trumpet',
         correct: '3', //Bassoon
-        message: `<p>It's the Bassoon!</p>`
+        message: `<img class="quiz-img" src="assets/images/bassoon-player.jpg" alt="Someone Playing the Bassoon"><p>It's the Bassoon!</p>`
     },
     {
         //8
-        question: `<h2>Which family is this instrument a member of?</h2>`,
+        question: `<h2>Which family is this instrument a member of?</h2><img class="quiz-img" src="assets/images/clarinet.jpg" alt="Clarinet>`,
         answer1: 'The Brass Family',
         answer2: 'The Percussion Family',
         answer3: 'The String Family',
         answer4: 'The Woodwind Family',
         correct: '4', //Clarinet - The Woodwind Family
-        message: `<p>The Clarinet is a member of The Woodwind Family!</p>`
+        message: `<img class="quiz-img" src="assets/images/clarinet-player.jpg" alt="Someone Playing the Clarinet"><p>The Clarinet is a member of The Woodwind Family!</p>`
     },
     {
         //Brass Instruments
         //9
-        question: `<h2>Which of these instruments sounds the highest?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        question: `<h2>Which of these instruments sounds the highest?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Trumpet',
         answer2: 'Trombone',
         answer3: 'French Horn',
         answer4: 'Tuba',
         correct: '1', //Trumpet
-        message: `<p>It's the Trumpet!</p>`
+        message: `<img class="quiz-img" src="assets/images/trumpet-player.jpg" alt="Someone Playing the Trumpet"><p>It's the Trumpet!</p>`
     },
     {
         //10
-        question: `<h2>Can you guess the name of this instrument?</h2>`,
+        question: `<h2>Can you guess the name of this instrument?</h2><img class="quiz-img" src="assets/images/french-horn.jpg" alt="French Horn">`,
         answer1: 'English Horn',
         answer2: 'Trumpet',
         answer3: 'French Horn',
         answer4: 'Euphonium',
         correct: '3', //French Horn
-        message: `<p>It's the French Horn!</p>`
+        message: `<img class="quiz-img" src="assets/images/french-horn-player.jpg" alt="Someone Playing the French Horn"><p>It's the French Horn!</p>`
     },
     {
         //11
-        question: `<h2>Which of these instruments is played with a slide?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        question: `<h2>Which of these instruments is played with a slide?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Tuba',
         answer2: 'Trombone',
         answer3: 'French Horn',
         answer4: 'Trumpet',
         correct: '2', //Trombone
-        message: `<p>It's the Trombone!</p>`
+        message: `<img class="quiz-img" src="assets/images/trombone-player.jpg" alt="Someone Playing the Trombone"><p>It's the Trombone!</p>`
     },
     {
         //12
-        question: `<h2>Which of these instruments sounds the lowest?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        question: `<h2>Which of these instruments sounds the lowest?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Trombone',
         answer2: 'Euphonium',
         answer3: 'Tuba',
         answer4: 'Trumpet',
         correct: '3', //Tuba
-        message: `<p>Its the Tuba!</p>`
-    },
-    {
-        //13
-        question: `<h2>Which family is this instrument a member of?</h2>`,
-        answer1: 'The String Family',
-        answer2: 'The Percussion Family',
-        answer3: 'The Woodwind Family',
-        answer4: 'The Brass Family',
-        correct: '4', //Euphonium - The Brass Family
-        message: `<p>The Euphonium is a member of The Brass Family!</p>`
+        message: `<img class="quiz-img" src="assets/images/tuba.jpg" alt="Tuba"><p>Its the Tuba!</p>`
     },
     {
         //Percussion Instruments
-        //14
-        question: `<h2>Which of these instruments is played using sticks?</h2>`,
+        //13
+        question: `<h2>Which of these instruments is played using sticks?</h2><img class="quiz-img" src="assets/images/drum-sticks.jpg" alt="Drum Sticks">`,
         answer1: 'Harp',
         answer2: 'Snare Drum',
         answer3: 'Piano',
         answer4: 'Flute',
         correct: '2', //Snare Drum
-        message: `<p>It's the Snare Drum!</p>`
+        message: `<img class="quiz-img" src="assets/images/snare-drum.jpg" alt="Snare Drum"><p>It's the Snare Drum!</p>`
     },
     {
-        //15
-        question: `<h2>Can you guess the name of this instrument?</h2>`,
+        //14
+        question: `<h2>Can you guess the name of this instrument?</h2><img class="quiz-img" src="assets/images/timpani.jpg" alt="Timpani">`,
         answer1: 'Timpani',
         answer2: 'Bass Drum',
         answer3: 'Euphonium',
         answer4: 'Snare Drum',
         correct: '1', //Timpani
-        message: `<p>It's the Timpani!</p>`
+        message: `<img class="quiz-img" src="assets/images/timpani.jpg" alt="Timpani"><p>It's the Timpani!</p>`
     },
     {
-        //16
-        question: `<h2>Which of these instruments is a tuned percussion instrument?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        //15
+        question: `<h2>Which of these instruments is a tuned percussion instrument?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Snare Drum',
         answer2: 'Xylophone',
         answer3: 'Cymbals',
         answer4: 'Bass Drum',
         correct: '2', //Xylophone
-        message: `<p>It's the Xylophone!</p>`
+        message: `<img class="quiz-img" src="assets/images/xylophone.jpg" alt="Xylophone"><p>It's the Xylophone!</p>`
     },
     {
-        //17
-        question: `<h2>Can you guess the name of this instrument?</h2>`,
+        //16
+        question: `<h2>Can you guess the name of this instrument?</h2><img class="quiz-img" src="assets/images/bass-drum.jpg" alt="Concert Bass Drum">`,
         answer1: 'Bass Drum',
         answer2: 'Snare Drum',
         answer3: 'Timpani',
         answer4: 'Cymbals',
         correct: '1', //Bass Drum
-        message: `<p>It's the Bass Drum!</p>`
+        message: `<img class="quiz-img" src="assets/images/bass-drum.jpg" alt="Concert Bass Drum"><p>It's the Bass Drum!</p>`
     },
     {
-        //18
-        question: `<h2>Which family is this instrument a member of?</h2>`,
+        //17
+        question: `<h2>Which family is this instrument a member of?</h2><img class="quiz-img" src="assets/images/cymbals.jpg" alt="Cymbals">`,
         answer1: 'The Woodwind Family',
         answer2: 'The Brass Family',
         answer3: 'The Percussion Family',
         answer4: 'The String Family',
         correct: '3', //Cymbals - The Percussion Family
-        message: `<p>The Cymbals are a member of The Percussion Family!</p>`
+        message: `<img class="quiz-img" src="assets/images/cymbal-players.jpg" alt="Cymbal Players"><p>The Cymbals are a member of The Percussion Family!</p>`
     },
     {
         //Additional Questions
-        //19
-        question: `<h2>Which of these instruments has black and white keys?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        //18
+        question: `<h2>Which of these instruments has black and white keys?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Harp',
         answer2: 'Piano',
         answer3: 'Clarinet',
         answer4: 'Oboe',
         correct: '2', //Piano
-        message: `<p>It's the Piano!</p>`
+        message: `<img class="quiz-img" src="assets/images/piano-player.jpg" alt="Pianist"><p>It's the Piano!</p>`
     },
     {
-        //20
-        question: `<h2>Which of these instruments is not used in an orchestra?</h2><img class="quiz-img" src="assets/images/question.png" alt="Question Mark">`,
+        //19
+        question: `<h2>Which of these instruments is not used in an orchestra?</h2><img class="quiz-icon" src="assets/images/icons/what.png" alt="Question Mark">`,
         answer1: 'Viola',
         answer2: 'Cornet',
         answer3: 'Bass Clarinet',
         answer4: 'Guitar',
         correct: '4', //Guitar
-        message: `<p>It's the Guitar!</p>`
+        message: `<img class="quiz-img" src="assets/images/guitar-player.jpg" alt="Guitarist"><p>It's the Guitar!</p>`
     }
 ]
